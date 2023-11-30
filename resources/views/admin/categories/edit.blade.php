a80bb09dfac349ff6b8bfbd3fe71faf9807ef2d8
Bart <87521270+bart2004@users.noreply.github.com>
2023-11-30 21:20:32 +0100

    style change for category edit

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -2,53 +2,58 @@
 @extends('layouts.admin')
 
 @section('content')
+<div class="d-flex justify-content-between align-items-center my-4">
+	<h4>Categorieën</h4>
+		<div>
+			<a href="{{ route('admin.categories.create') }}">Nieuwe categorie toevoegen</a>
+		</div>
+</div>
+<div class="d-flex justify-content-between align-items-center my-4">
+	
+	<div class="d-flex align-items-center flex-column my-5"> 
 
-<div class="d-flex align-items-center flex-column my-5"> 
+		<form action="{{ route('admin.categories.update', $category) }}" method="POST" style="min-width: 320px;">
+			
+			<h4>Categorie aanpassen</h4>
 
-	<form action="{{ route('admin.categories.update', $category) }}" method="POST" style="min-width: 320px;">
+			<div class="form-group">
+				<label for="name">Naam</label>
+				<input type="text" id="name" name="name" class="form-control" value="{{ old('name', $category->name) }}">
+			</div>
+
+			<div class="form-group my-4">
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" name="active" id="active1" value="1" @if(old('active', $category->active)) checked @endif>
+					<label class="form-check-label" for="active1">Zichtbaar</label>
+				</div>
+				<div class="form-check form-check-inline">
+					<input class="form-check-input" type="radio" name="active" id="active0" value="0" @if(!old('active', $category->active)) checked @endif>
+					<label class="form-check-label" for="active0">Onzichtbaar</label>
+				</div>
+			</div>
+
+			<button type="submit" class="form-control btn btn-primary my-2">Opslaan</button>
+			{{ csrf_field() }}
+			{{ method_field('PATCH') }}
+		</form>
+		<form action="{{ route('admin.categories.destroy', $category) }}" method="POST">
+			<button type="submit" class="form-control btn btn-outline-danger">Verwijderen</button>
+			{{ csrf_field() }}
+			{{ method_field('DELETE') }}
+		</form>
 		
-		<h4>Categorie aanpassen</h4>
+	</div>
 
-		<div class="form-group">
-			<label for="name">Naam</label>
-			<input type="text" id="name" name="name" class="form-control" value="{{ old('name', $category->name) }}">
-		</div>
-
-		<div class="form-group my-4">
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" name="active" id="active1" value="1" @if(old('active', $category->active)) checked @endif>
-				<label class="form-check-label" for="active1">Zichtbaar</label>
-			</div>
-			<div class="form-check form-check-inline">
-				<input class="form-check-input" type="radio" name="active" id="active0" value="0" @if(!old('active', $category->active)) checked @endif>
-				<label class="form-check-label" for="active0">Onzichtbaar</label>
-			</div>
-		</div>
-
-		<button type="submit" class="form-control btn btn-primary my-2">Opslaan</button>
-		{{ csrf_field() }}
-		{{ method_field('PATCH') }}
-	</form>
-	<form action="{{ route('admin.categories.destroy', $category) }}" method="POST">
-		<button type="submit" class="form-control btn btn-outline-danger">Verwijderen</button>
-		{{ csrf_field() }}
-		{{ method_field('DELETE') }}
-	</form>
+	<!-- displays every product in the category -->
+	<div class="my-5">
+		<h4>Products in {{ $category->name }}</h4>
+		<ul>
+			@foreach($category->products as $product)
+			<li>{{ $product->title }}</li>
+			@endforeach
+		</ul>
+	</div>
 </div>
-
-//code below isnt working properly yet
-// it schould display every product in the category by category id
-	
-// Add the code below to display the list of products with the selected category
-<div class="my-5">
-	<h4>Products in {{ $category->name }}</h4>
-	<ul>
-		@foreach($category->products as $product)
-		<li>{{ $product->title }}</li>
-        @endforeach
-	</ul>
-</div>
-	
 
 
 @endsection
